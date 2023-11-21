473d901c478a4884db1f6868a6cc1841eb8a402a
Simon <simon.louis638@gmail.com>
2023-11-21 08:32:26 +0100

    gestion des différents traffics

--- C#/Traffic.cs
+++ C#/Traffic.cs
@@ -7,5 +7,20 @@ namespace TrafficSimulation
         {
             public TypeIntersection Type { get; set; }
 
+            public void GérerTrafic(Vehicule véhicule)
+            {
+                switch (Type)
+                {
+                    case TypeIntersection.CedezLePassage:
+                        GérerIntersectionCedezLePassage(véhicule);
+                        break;
+                    case TypeIntersection.RondPoint:
+                        GérerIntersectionRondPoint(véhicule);
+                        break;
+                    case TypeIntersection.FeuxTricolores:
+                        GérerIntersectionFeuxTricolores(véhicule);
+                        break;
+                }
+            }
         }
     }
