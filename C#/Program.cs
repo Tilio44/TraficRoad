bf5e8ec3f99ab6f3f3c59445e3a3e31b3f25b298
Simon <simon.louis638@gmail.com>
2023-11-26 17:27:27 +0100

    Projet final

--- C#/Program.cs
+++ C#/Program.cs
@@ -1,15 +1,46 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 class Program
 {
+    static void Main()
+    {
+        TrafficSimulation.Traffic.Intersection intersectionCeder = new TrafficSimulation.Traffic.Intersection { Type = TrafficSimulation.Traffic.TypeIntersection.CedezLePassage };
+        TrafficSimulation.Traffic.Intersection rondPointIntersection = new TrafficSimulation.Traffic.Intersection { Type = TrafficSimulation.Traffic.TypeIntersection.RondPoint };
+        TrafficSimulation.Traffic.Intersection feuxTricoloresIntersection = new TrafficSimulation.Traffic.Intersection { Type = TrafficSimulation.Traffic.TypeIntersection.FeuxTricolores };
+
+        Random aléatoire = new Random();
+
+        while (true)
+        {
+            int nombreVoitures = aléatoire.Next(5) + 1; // Nombre aléatoire de voitures entre 1 et 5
+            int nombrePiétons = aléatoire.Next(3) + 1; // Nombre aléatoire de piétons entre 1 et 3
+
+            Console.WriteLine($"----- Nouvelle itération - {nombreVoitures} voitures, {nombrePiétons} piétons -----");
+
+            for (int i = 0; i < nombreVoitures; i++)
+            {
+                TrafficSimulation.Traffic.Vehicule voiture = new TrafficSimulation.Traffic.Vehicule { Type = "Voiture" };
+                EffectuerActionAléatoire(voiture, intersectionCeder, rondPointIntersection, feuxTricoloresIntersection, aléatoire);
+            }
+
+            for (int i = 0; i < nombrePiétons; i++)
+            {
+                TrafficSimulation.Traffic.Vehicule piéton = new TrafficSimulation.Traffic.Vehicule { Type = "Piéton" };
+                EffectuerActionAléatoire(piéton, intersectionCeder, rondPointIntersection, feuxTricoloresIntersection, aléatoire);
+            }
+
+            Thread.Sleep(10000); // Attente de 10 secondes entre chaque itération
+        }
+    }
 
     static void EffectuerActionAléatoire(
         TrafficSimulation.Traffic.Vehicule véhicule,
         TrafficSimulation.Traffic.Intersection intersectionCeder,
         TrafficSimulation.Traffic.Intersection rondPointIntersection,
         TrafficSimulation.Traffic.Intersection feuxTricoloresIntersection,
-        Random aléatoire
-    )
+        Random aléatoire)
     {
         véhicule.EffectuerAction("Approche de l'intersection");
 
--- C#/Traffic.cs
+++ C#/Traffic.cs
@@ -2,6 +2,24 @@ using System;
 
 namespace TrafficSimulation
 {
+    public class Traffic
+    {
+        public enum TypeIntersection
+        {
+            CedezLePassage,
+            RondPoint,
+            FeuxTricolores
+        }
+
+        public class Vehicule
+        {
+            public string Type { get; set; }
+
+            public void EffectuerAction(string action)
+            {
+                Console.WriteLine($"{Type} {action}");
+            }
+        }
 
         public class Intersection
         {
@@ -22,5 +40,32 @@ namespace TrafficSimulation
                         break;
                 }
             }
+
+            private void GérerIntersectionCedezLePassage(Vehicule véhicule)
+            {
+                // Logique de gestion pour une intersection céder le passage
+                véhicule.EffectuerAction("Arrivée à l'intersection céder le passage / arrivée au passage piéton");
+                véhicule.EffectuerAction("Céder le passage aux autres véhicules / passage piéton vert");
+                véhicule.EffectuerAction("Entrée dans l'intersection / traverse le passage piéton");
+                véhicule.EffectuerAction("Sortie de l'intersection / sortie du passage piéton");
+            }
+
+            private void GérerIntersectionRondPoint(Vehicule véhicule)
+            {
+                // Logique de gestion pour un rond-point / passage piéton
+                véhicule.EffectuerAction("Arrivée au rond-point / arrivée au passage piéton");
+                véhicule.EffectuerAction("Navigation dans le rond-point / traverse le passage piéton");
+                véhicule.EffectuerAction("Sortie du rond-point / sortie du passage piéton");
+            }
+
+            private void GérerIntersectionFeuxTricolores(Vehicule véhicule)
+            {
+                // Logique de gestion pour une intersection avec feux tricolores
+                véhicule.EffectuerAction("Arrivée à l'intersection avec feux tricolores / arrivée au passage piéton");
+                véhicule.EffectuerAction("Attente du feu vert / passage piéton vert");
+                véhicule.EffectuerAction("Passer l'intersection / traverser le passage piéton");
+                véhicule.EffectuerAction("Sortie de l'intersection avec feux tricolores / sortie passage piéton");
+            }
         }
     }
+}
